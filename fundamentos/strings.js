2c40777ea412593acb912c73630e6316424c7b00
Jean Henrique dos Santos Nonato <jean.nonato@IMAC004.local>
2018-04-06 15:51:57 -0300

    Examples of strings

--- fundamentos/strings.js
+++ fundamentos/strings.js
@@ -2,4 +2,4 @@ const escola = "Cod3r"
 
 console.log(escola.charAt(4))  //charAt vai dar a letra que está no indice 4 da string
 console.log(escola.charAt(5))
-console.log(escola.charCodeAt(3)) //Pega o valor do caracter na tabela ASCII ou UNICODE
+console.log(escola.charCodeAt(3)) //Pega o valor do caracter na tabela ASCII ou UNICODE 
