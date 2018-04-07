ef0557e3b30b3ce0053bb266ca5a4e0496bc8d09
jean.henriq <jean.henriq@live.com>
2018-04-07 14:11:24 -0300

    More examples about numbers care

--- fundamentos/numerosAlgunsCuidados.js
+++ fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,6 @@
 console.log(7 / 0)
 console.log("10.2" / 2)
+console.log("2" + 4) //Não realiza soma, como o + é utilizado em concatenação, a string ganha contra a soma. Logo vira 24 e não 6
 console.log("Show"* 2)
 console.log(0.1 + 0.7)
 console.log((10.35234).toFixed(2)) //É possivel chamar o literal dentro dos parenteses pra evitar salvar numa var e precisar chamar a var quando for chamar função 
